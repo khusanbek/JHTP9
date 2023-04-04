024a437aa2a13d94b35073e2c08b1d5c35bd4ffe
Khusanbek Gafurov <x.gafurov@gmail.com>
2023-04-04 18:39:48 +0500

    Add files via upload

---
+++ Keypad.java
@@ -0,0 +1,17 @@
+// Keypad.java
+import java.util.Scanner;
+
+public class Keypad
+{
+	private Scanner input;
+	
+	public Keypad()
+	{
+		input = new Scanner(System.in);
+	}
+	
+	public int getInput()
+	{
+		return input.nextInt();
+	}
+}
