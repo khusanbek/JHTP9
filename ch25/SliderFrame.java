ddca41ac598b1b2d2fcca6605cd8376595c3f033
Khusanbek Gafurov <x.gafurov@gmail.com>
2023-04-15 00:47:55 +0500

    Create SliderFrame.java

---
+++ ch25/SliderFrame.java
@@ -0,0 +1,49 @@
+// SliderFrame.java
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+
+public class SliderFrame extends JFrame
+{
+	private JSlider diameterJSlider;
+	private OvalPanel myPanel;
+	
+	public SliderFrame()
+	{
+		super("Slider Demo");
+		
+		myPanel = new OvalPanel();
+		myPanel.setBackground(Color.YELLOW);
+		
+		diameterJSlider = 
+			new JSlider(SwingConstants.HORIZONTAL, 0, 200, 10);
+		diameterJSlider.setMajorTickSpacing(10);
+		diameterJSlider.setPaintTicks(true);
+		
+		diameterJSlider.addChangeListener(
+			
+			new ChangeListener()
+			{
+				public void stateChanged(ChangeEvent e)
+				{
+					myPanel.setDiameter(diameterJSlider.getValue());
+				}
+			}
+		);
+		
+		add(diameterJSlider, BorderLayout.SOUTH);
+		add(myPanel, BorderLayout.CENTER);
+	}
+	
+	public static void main(String[] args)
+	{
+		SliderFrame sliderFrame = new SliderFrame();
+		sliderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sliderFrame.setSize(220, 270);
+		sliderFrame.setVisible(true);
+	}
+}
