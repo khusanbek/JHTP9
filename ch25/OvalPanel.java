f76367634b7efd1aab6ca2d578d98d049271c303
Khusanbek Gafurov <x.gafurov@gmail.com>
2023-04-15 00:48:17 +0500

    Add files via upload

---
+++ ch25/OvalPanel.java
@@ -0,0 +1,32 @@
+// OvalPanel.java
+import java.awt.Graphics;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class OvalPanel extends JPanel
+{
+	private int diameter = 10;
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		g.fillOval(10, 10, diameter, diameter);
+	}
+	
+	public void setDiameter(int newDiameter)
+	{
+		diameter = (newDiameter >= 0 ? newDiameter : 10);
+		repaint();
+	}
+	
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(200, 200);
+	}
+	
+	public Dimension getMinimumSize()
+	{
+		return getPreferredSize();
+	}
+}
