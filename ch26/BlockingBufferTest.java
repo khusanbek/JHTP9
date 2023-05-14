a09a3c2807de185cebc458a4fd4b0aa5e3e40a78
Khusanbek Gafurov <x.gafurov@gmail.com>
2023-05-14 20:52:37 +0500

    Add files via upload

---
+++ ch26/BlockingBufferTest.java
@@ -0,0 +1,18 @@
+// BlockingBufferTest.java
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BlockingBufferTest
+{
+	public static void main(String[] args)
+	{
+		ExecutorService application = Executors.newCachedThreadPool();
+		
+		Buffer sharedLocation = new BlockingBuffer();
+		
+		application.execute(new Producer(sharedLocation));
+		application.execute(new Consumer(sharedLocation));
+		
+		application.shutdown();
+	}
+}
