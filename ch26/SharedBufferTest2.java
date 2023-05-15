1e3a2b71a050f7860f5fab5ef481609980899491
Khusanbek Gafurov <x.gafurov@gmail.com>
2023-05-15 15:13:12 +0500

    Add files via upload

---
+++ ch26/SharedBufferTest2.java
@@ -0,0 +1,21 @@
+// SharedBufferTest2.java
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SharedBufferTest2
+{
+	public static void main(String[] args)
+	{
+		ExecutorService application = Executors.newCachedThreadPool();
+		
+		Buffer sharedLocation = new SynchronizedBuffer();
+		
+		System.out.printf("%-40s%s\t\t%s\n%-40s%s\n\n", "Operation",
+		   "Buffer", "Occupied", "---------", "------\t\t--------");
+		   
+		application.execute(new Producer(sharedLocation));   
+		application.execute(new Consumer(sharedLocation));
+
+		application.shutdown();		
+	}
+}
